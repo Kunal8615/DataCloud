93247956784cdd3b289a360af7309777f3353365
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2024-09-16 19:59:17 +0530

    temp fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,11 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 
-app.use(cors(
-    {
-        origin : "*",
-        credentials : true
-    }
-));
+app.use(cors({
+    origin: ["http://localhost:5173",], // Multiple origins allowed
+    credentials: true
+}));
+
 
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ extended: true, limit: "30mb" }));
