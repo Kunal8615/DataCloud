8a96687f272f05a1eb78c21c6ec10996b37ae9ad
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2024-09-20 18:32:27 +0530

    design fix

--- frontend/src/pages/Recent.jsx
+++ frontend/src/pages/Recent.jsx
@@ -78,8 +78,8 @@ const Recent = () => {
               <iframe 
                 src={getFilePreview(file.dataFile)} 
                 title={file.title} 
-                className="w-72 h-40 object-cover mb-4 rounded-md" 
-                frameBorder="0"
+                className="min-w-52 h-40 object-cover mb-4 rounded-md" 
+               
               ></iframe>
             ) : (
               <img 
